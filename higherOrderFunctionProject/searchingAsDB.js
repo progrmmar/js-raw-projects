7f32b1f26c19efc33e2289d5fb4e873062d8b1cb
progrmmar <atecharjun10@gmail.com>
2026-02-01 13:40:21 +0600

    I have created a new searching algorithm like db search

---
+++ higherOrderFunctionProject/searchingAsDB.js
@@ -0,0 +1,32 @@
+/*
+    Project Name: Filterring some objects from a array by another obejcts.
+    Author: Arjun Roy 
+    Description: Fikterring a objrct from an array eith a payload which can be with one key or muli-key object
+    Date: 01-02-2036
+*/
+
+const whatIsInAName = (catalog, payload) => {
+    const payloadKeys = Object.keys(payload);
+
+    const filteredCatalog = catalog.filter(obj => {
+        const isAllValid = payloadKeys.map(payloadKey => {
+            return (
+                Object.keys(obj).includes(payloadKey) &&
+                payload[payloadKey] === obj[payloadKey]
+            );
+        });
+
+        return isAllValid.includes(false) ? false : true;
+    });
+
+    return filteredCatalog;
+};
+
+/*console.log(
+    whatIsInAName(
+        [{ apple: 1, bat: 2 }, { apple: 1 }, { apple: 1, bat: 2, cookie: 2 }],
+        { apple: 1, cookie: 2 }
+    )
+);*/
+
+// console.log(whatIsInAName([], ));
