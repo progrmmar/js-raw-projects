b22c2fcd28d99b8749114da8101cccc306b6535d
progrmmar <atecharjun10@gmail.com>
2026-02-01 14:31:47 +0600

    I have built a new app for adding prime nums under provided all nums

---
+++ higherOrderFunctionProject/sumPrimes.js
@@ -0,0 +1,32 @@
+/*
+    Project Name: sumPrimes
+    Author: Arjun Roy 
+    Description: Adding the prime under the provided number
+    Date: 01-02-2036
+*/
+
+const sumPrimes = number => {
+    const primeNumbers = [];
+
+    if (number === 2) return number;
+
+    let derivative = [];
+    for (let n = 2; n <= number; n++) {
+        for (let i = 1; i <= n; i++) {
+            if (n % i === 0) {
+                derivative.push(i);
+            }
+        }
+        if (derivative.length <= 2) {
+            primeNumbers.push(n);
+        }
+        derivative = [];
+    }
+
+    const total = primeNumbers.reduce((total, primeNum) => {
+        return total + primeNum;
+    });
+
+    return total;
+};
+console.log(sumPrimes(5));
